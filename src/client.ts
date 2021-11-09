bea62d79827f0524d4adb1e8aade99d32cbba1a8
Sovattha Sok <github@soks.fr>
2021-11-10 00:45:04 +0100

    Add rxjs and websockets

--- src/client.ts
+++ src/client.ts
@@ -1,51 +1,81 @@
-import WebSocket from "ws";
+import { catchError, fromEvent, map, merge, mergeMap, Observable, tap } from 'rxjs';
+import WebSocket, { MessageEvent } from 'ws';
 
 (async () => {
-  const ws1 = await connectToWebsocket("wss://api.gateio.ws/ws/v4/", 'spot.tickers', ["ETH_USDT"]);
-  const ws2 = await connectToWebsocket("wss://api.gateio.ws/ws/v4/", 'spot.tickers', ["BTC_USDT"]);
-  const ws3 = await connectToWebsocket("ws://localhost:8080", 'orders', {"pair": "ETH_USDT","price":2000,"amount":0.2});
 
-  ws1.on("message", function message(data: WebSocket.RawData) {
-    const response: any = JSON.parse(data.toString());
-    switch (response.channel) {
-      case "spot.tickers":
-        // console.log(`${response.result.currency_pair} ${response.result.last}`);
-        ws3.send(`${response.result.currency_pair} ${response.result.last}`);
-        break;
-      default: 
-        console.log("received: %s", response.channel);
-    }
-  });
-  ws3.on("message", function message(data: WebSocket.RawData) {
-    console.log("received: %s", data);
-  });
+  const ws0 = await connectToWebsocket('wss://api.gateio.ws/ws/v4/', 'spot.tickers', ['LTC_USDT']);
+  const ws1 = await connectToWebsocket('wss://api.gateio.ws/ws/v4/', 'spot.tickers', ['ETH_USDT']);
+  const ws2 = await connectToWebsocket('wss://api.gateio.ws/ws/v4/', 'spot.tickers', ['BTC_USDT']);
+  const ws3 = await connectToWebsocket('ws://localhost:8080', 'orders');
+
+  const ws0$ = (fromEvent(ws0, 'message') as Observable<MessageEvent>);
+  const ws1$ = (fromEvent(ws1, 'message') as Observable<MessageEvent>);
+  const ws2$ = (fromEvent(ws2, 'message') as Observable<MessageEvent>);
+  const ws3$ = (fromEvent(ws3, 'message') as Observable<MessageEvent>);
+
+  const ws4$ = merge(ws0$, ws1$, ws2$, ws3$).pipe(
+    // tap((value) => console.log(value.data)),
+    tap((value) => {
+      const response: any = JSON.parse(value.data.toString());
+      switch (response.channel) {
+        case 'spot.tickers':
+          console.log(`${response.channel} = ${response.result?.currency_pair || ''} - ${response.result?.last || ''} ${response.result?.status || ''}`);
+          if (response.result?.currency_pair) {
+            ws3.send(JSON.stringify({channel: 'orders', payload: { pair: response.result?.currency_pair, price: response.result?.last, amount: 1} }));
+          }
+          break;
+        case 'orders':
+          console.log(`${response.channel} = ${response.payload?.pair} ${response.payload?.price} ${response.payload?.amount}`);
+          break;
+        default: 
+          console.log('received: %s', response);
+      }
+    }),
+    catchError((err, caught) => {
+      console.log('error: ', err)
+      return caught;
+    }),
+  );
+  ws4$.subscribe();
+  
 })();
 
-async function connectToWebsocket(wsUrl: string, channel: string, payload: any) {
+async function connectToWebsocket(wsUrl: string, channel: string, payload?: any) {
+  const ws = new WebSocket(wsUrl);
   return new Promise<WebSocket>((resolve, reject) => {
-    const ws = new WebSocket(wsUrl);
-
-    ws.on("open", function open() {
-      console.log('subscribing', channel)
-      ws.send(
-        JSON.stringify({
-          time: Math.round(new Date().getTime() / 1000),
-          channel: "spot.tickers",
-          event: "subscribe",
-          payload: ["BTC_USDT"],
-        })
-      );
-      console.log('subscribed', channel);
-      ws.send(
-        JSON.stringify({
-          time: Math.round(new Date().getTime() / 1000),
-          channel: channel,
-          event: "subscribe",
-          payload,
-        })
-      );
-      resolve(ws);
-    });
+    if (payload) {
+      ws.on('open', function open() {
+        console.log('subscribing', channel, payload)
+        ws.send(
+          JSON.stringify({
+            time: Math.round(new Date().getTime() / 1000),
+            channel: channel,
+            event: 'subscribe',
+            payload,
+          })
+        );
+        console.log('subscribed', channel, payload);
+        resolve(ws);
+      });
+    } else resolve(ws);
   });
+}
 
+const handleConnection = (myWs: WebSocket): Observable<MessageEvent | CloseEvent> => {
+  const onMessage$ = (fromEvent(myWs, 'message') as Observable<MessageEvent>)
+    .pipe(
+      map(message => JSON.parse(message.data.toString())),
+      tap(messageValue => console.log('message', messageValue)),
+      catchError((err, caught) => {
+        console.log('error: ', err)
+        return caught;
+      }),
+    );
+  const onClose$ = (fromEvent(myWs, 'close') as Observable<CloseEvent>)
+    .pipe(
+      tap(closeMessage => console.log('reason', closeMessage.reason)));
+  const onError$ = (fromEvent(myWs, 'error') as Observable<ErrorEvent>)
+      .pipe(
+        tap(errorMessage => console.log('error', errorMessage.error)));
+  return merge(onMessage$, onClose$, onError$)
 }
--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,6 @@ const wss = new WebSocketServer({ port: 8080 });
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
-    ws.send(message);
+    ws.send(message.toString('utf-8'));
   });
-
-  ws.send('connection ok');
 });
