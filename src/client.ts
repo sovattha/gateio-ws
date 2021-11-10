469a0866790a4aaa6bb15a47813805ec2bd2a120
Sovattha Sok <github@soks.fr>
2021-11-10 01:06:42 +0100

    Refactor to speedup and use rxjs better

--- src/client.ts
+++ src/client.ts
@@ -3,40 +3,39 @@ import WebSocket, { MessageEvent } from 'ws';
 
 (async () => {
 
-  const ws0 = await connectToWebsocket('wss://api.gateio.ws/ws/v4/', 'spot.tickers', ['LTC_USDT']);
-  const ws1 = await connectToWebsocket('wss://api.gateio.ws/ws/v4/', 'spot.tickers', ['ETH_USDT']);
-  const ws2 = await connectToWebsocket('wss://api.gateio.ws/ws/v4/', 'spot.tickers', ['BTC_USDT']);
+  const ws0 = connectToWebsocket('wss://api.gateio.ws/ws/v4/', 'spot.tickers', ['LTC_USDT']);
+  const ws1 = connectToWebsocket('wss://api.gateio.ws/ws/v4/', 'spot.tickers', ['ETH_USDT']);
+  const ws2 = connectToWebsocket('wss://api.gateio.ws/ws/v4/', 'spot.tickers', ['BTC_USDT']);
   const ws3 = await connectToWebsocket('ws://localhost:8080', 'orders');
+  const [ws0$, ws1$, ws2$, ws3$] = await Promise.all([ws0, ws1, ws2, ws3]).then((data) => 
+    Promise.all(data.map(w => fromEvent(w, 'message') as Observable<MessageEvent>))
+  );
 
-  const ws0$ = (fromEvent(ws0, 'message') as Observable<MessageEvent>);
-  const ws1$ = (fromEvent(ws1, 'message') as Observable<MessageEvent>);
-  const ws2$ = (fromEvent(ws2, 'message') as Observable<MessageEvent>);
-  const ws3$ = (fromEvent(ws3, 'message') as Observable<MessageEvent>);
+  const ws5$ = ws3$.pipe(
+    map((value) => JSON.parse(value.data.toString()) as any),
+    tap((response) => {
+      console.log(`${response.channel} = ${response.payload?.pair} ${response.payload?.price} ${response.payload?.amount}`);
+    }),
+  );
 
-  const ws4$ = merge(ws0$, ws1$, ws2$, ws3$).pipe(
-    // tap((value) => console.log(value.data)),
-    tap((value) => {
-      const response: any = JSON.parse(value.data.toString());
-      switch (response.channel) {
-        case 'spot.tickers':
-          console.log(`${response.channel} = ${response.result?.currency_pair || ''} - ${response.result?.last || ''} ${response.result?.status || ''}`);
-          if (response.result?.currency_pair) {
-            ws3.send(JSON.stringify({channel: 'orders', payload: { pair: response.result?.currency_pair, price: response.result?.last, amount: 1} }));
-          }
-          break;
-        case 'orders':
-          console.log(`${response.channel} = ${response.payload?.pair} ${response.payload?.price} ${response.payload?.amount}`);
-          break;
-        default: 
-          console.log('received: %s', response);
+  const ws4$ = merge(ws0$, ws1$, ws2$).pipe(
+    map((value) => JSON.parse(value.data.toString()) as any),
+    tap((response) => {
+      console.log(`${response.channel} = ${response.result?.currency_pair || ''} - ${response.result?.last || ''} ${response.result?.status || ''}`);
+    }),
+    tap((response) => {
+      if (response.result?.currency_pair) {
+        ws3.send(JSON.stringify({channel: 'orders', payload: { pair: response.result?.currency_pair, price: response.result?.last, amount: 1} }));
       }
     }),
+  );
+  
+  merge(ws5$, ws4$).pipe(
     catchError((err, caught) => {
       console.log('error: ', err)
       return caught;
     }),
-  );
-  ws4$.subscribe();
+  ).subscribe();
   
 })();
 
@@ -59,23 +58,4 @@ async function connectToWebsocket(wsUrl: string, channel: string, payload?: any)
       });
     } else resolve(ws);
   });
-}
-
-const handleConnection = (myWs: WebSocket): Observable<MessageEvent | CloseEvent> => {
-  const onMessage$ = (fromEvent(myWs, 'message') as Observable<MessageEvent>)
-    .pipe(
-      map(message => JSON.parse(message.data.toString())),
-      tap(messageValue => console.log('message', messageValue)),
-      catchError((err, caught) => {
-        console.log('error: ', err)
-        return caught;
-      }),
-    );
-  const onClose$ = (fromEvent(myWs, 'close') as Observable<CloseEvent>)
-    .pipe(
-      tap(closeMessage => console.log('reason', closeMessage.reason)));
-  const onError$ = (fromEvent(myWs, 'error') as Observable<ErrorEvent>)
-      .pipe(
-        tap(errorMessage => console.log('error', errorMessage.error)));
-  return merge(onMessage$, onClose$, onError$)
 }
